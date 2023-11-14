8030aa45254dd80c2ce60ca9a3b665f87dd7e805
kevin <kevinup13@gmail.com>
2023-11-14 01:41:23 -0400

    update icons, light mode and other functions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,34 @@
 // Initial data
+const changeThemeBtn = document.querySelector("#change-theme");
+const $html = document.querySelector("html");
 
 // Functions
+function toggleTheme() {
+    $html.classList.toggle("light");
+}
+function verifyTheme() {
+    let textSpan = document.getElementById("textTheme")
+    if ($html.classList.contains("light")) {
+        textSpan.innerHTML = "Light";
+    } else {
+        textSpan.innerHTML = "Dark";
+    }
+}
+function loadTheme() {
+    let theme = localStorage.getItem("html");
+    $html.classList.add(theme);
+    verifyTheme();
+}
 
 // Events
+changeThemeBtn.addEventListener("change", () => {
+    toggleTheme();
+    verifyTheme();
+    // save theme
+    if ($html.classList.contains("light")) {
+        localStorage.setItem('html', 'light');
+    } else {
+        localStorage.setItem('html', '');
+    }
+})
+loadTheme();
